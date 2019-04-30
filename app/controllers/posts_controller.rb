84f40a408d862777e8b6cd18b1f16d02bad1b6d3
spatel1009 <spatel1009@gmail.com>
2019-04-30 15:16:52 -0400

    created show page and index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,26 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show]
+  
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
   end
-  
+
+  def create
+    @post = Post.create!(post_params)
+    redirect_to @post
+  end
+
+  def show
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
